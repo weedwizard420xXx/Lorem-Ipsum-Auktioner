341dd5bd49b9fc5ddef23b9bbb760a6448e2633b
weedwizard420xXx <72909471+weedwizard420xXx@users.noreply.github.com>
2021-06-09 15:08:15 +0200

    Update Brugerside.js
    Flyttet over til en ekstern side

--- React frontend/app/src/components/Brugerside.js
+++ React frontend/app/src/components/Brugerside.js
@@ -3,11 +3,7 @@ import '../App.css';
 import AppNavbar from './AppNavbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link, withRouter } from 'react-router-dom';
-import { Container, Button } from 'reactstrap';
-import socketIOClient from 'socket.io-client';
 import Api from '../api/Api';
-
-const socket = socketIOClient('http://localhost:8080');
 
 class Brugerside extends Component {
   
@@ -15,30 +11,13 @@ class Brugerside extends Component {
     
     super(props);
     this.state = {
-      auctions: [],
-      username: '',
-      items: [],
-      userBid: '',
-      bidAmount: '',
-      bidTime: '',
-      waiting: '',
-      loading: false,
-      bla: []
+      username: ''
     }
-    
-    this.auctionsBtnClick = this.auctionsBtnClick.bind(this);
-    this.bidOnItem = this.bidOnItem.bind(this);
     
   }
   
   componentDidMount() {
-
     this.checkAuth();
-
-    fetch('/api/getAuctions')
-      .then(response => response.json())
-      .then(data => this.setState({auctions: data}));
-
   }
 
   checkAuth() {
@@ -62,115 +41,15 @@ class Brugerside extends Component {
 
   }
 
-  auctionsBtnClick(auctionName, id) {
-
-    //fetcher bud hver gang nogen laver et bud
-    socket.on('bidUpdate', data => {
-
-      if(data.message === 'accepted') {
-        this.setState({
-          userBid: data.items[0].username, 
-          bidAmount: data.items[0].bud, 
-          bidTime: data.items[0].created
-        });
-      }
-      else {
-        this.setState({
-          userBid: '', 
-          bidAmount: '', 
-          bidTime: ''
-        });
-      }
-
-    });
-
-    const username = this.state.username;
-
-    socket.emit('join', {username: username, name: auctionName, auctionId: id});
-
-    socket.on('joined', data => {
-
-      
-      if(data.message === 'confirmed') {
-
-        if(data.items.length > 1) {
-          return this.setState({waiting: data.waiting, loading: true});
-        } 
-
-        data.items.map( item => {
-          console.log(item)
-          return this.setState({bla: <Button key={id} onClick={() => this.bidOnItem(username, auctionName, id, item.id)} className='btn btn-primary'>Byd</Button>, items: data.items, loading: false})
-        });
-        
-
-      }
-
-    });
-
-  }
-
-  bidOnItem(username, auctionName, id, itemId) {
-
-    socket.emit('bid', {user: username, name: auctionName, auctionId: id, itemId: itemId, bid: this.state.bidAmount});
-    console.log(username, auctionName, id, itemId)
-
-  }
-
-
 
   render() {
-    
-    const { username, auctions, items, userBid, bidAmount, bidTime, loading, waiting} = this.state;
-
-    const auctionList = auctions.map(auction => {
-
-      let buttons = [];
-      buttons.push(<Button className='btn btn-primary' color='primary' style={{marginRight: "5px"}} key={auction.id} onClick={() => this.auctionsBtnClick(auction.name, auction.id)}>{auction.name}</Button>);
-
-      return buttons;
-
-    });
-    
-    const currentItem = items.map( item => {
-      console.log(item)
-
-      const d = new Date(bidTime);
-
-      return (
-        <p key={item.id}>
-          {item.name}
-          <br/>
-          {item.description}
-          <br />
-          Nuværende bud: {bidAmount} <br/>
-          Bruger: {userBid} <br/>
-          Lavet kl. : {d.toLocaleString('da-DK')} 
-        </p>
-      )
-
-    });
 
     return (
       <div>
-        
         <AppNavbar />
-        <Container fluid>
-          <p>Velkommen {username}</p>
-          <Link className='btn btn-primary' to=''>Auktioner</Link>
+          <p>Velkommen {this.state.username}</p>
+          <Link className='btn btn-primary' to='/UserAuktioner'>Auktioner</Link>
           <Link className='btn btn-primary' to='/SendVurdering'>Indsend vare til vurdering</Link>
-          <div>
-          <br />
-            {auctionList}
-          </div>
-          <br />
-          <div>
-            {loading === true ? waiting : currentItem}
-          </div>
-          <br />
-          <div>
-            {this.state.bla}
-          </div>
-        </Container>
       </div>
     );
   }
